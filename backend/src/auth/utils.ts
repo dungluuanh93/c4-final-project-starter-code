58900b0d9a9994380138d29864da1e010a9d760a
dungluuanh93 <dungluuanh93@gmail.com>
2022-05-19 16:41:22 +0700

    new code

--- backend/src/auth/utils.ts
+++ backend/src/auth/utils.ts
@@ -11,3 +11,23 @@ export function parseUserId(jwtToken: string): string {
   const decodedJwt = decode(jwtToken) as JwtPayload
   return decodedJwt.sub
 }
+
+// export function getUserId(event: APIGatewayProxyEvent): string {
+//   const authorization = event.headers.Authorization
+//   const split = authorization.split(' ')
+//   const jwtToken = split[1]
+
+//   return parseUserId(jwtToken)
+// }
+
+export const getToken = (authHeader: string): string => {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}
--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,19 +1,37 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
+import { getSignedUploadUrl } from '../../businessLogic/todos'
+import { createLogger } from '../../utils/logger'
+import { getToken, parseUserId } from '../../auth/utils'
 
-import { createAttachmentPresignedUrl } from '../../businessLogic/todos'
-import { getUserId } from '../utils'
+const logger = createLogger('get-s3-url')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    const todoId = event.pathParameters.todoId
-    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-    
+    logger.info('get signed upload s3 url')
+    try {
+      const jwtToken = getToken(event.headers.Authorization)
+      const todoId = event.pathParameters.todoId
+      const userId = parseUserId(jwtToken)
+      logger.info('userId: ',userId)
+      // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
 
-    return undefined
+      const url: string = await getSignedUploadUrl(todoId, userId)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          uploadUrl: url
+        })
+      }
+    } catch (e) {
+      logger.error(e.message)
+      return {
+        statusCode: 500,
+        body: e.message
+      }
+    }
   }
 )
 
